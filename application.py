622a04e21b57f59b4ce899dfeaef0d5644e44b40
Vahan Mouradian <vahanmouradian@outlook.com>
2020-07-17 13:37:43 -0700

    test

--- application.py
+++ application.py
@@ -58,33 +58,31 @@ def serve_layout():
 
 
     
-# =============================================================================
-#     
-#     client =  paramiko.client.SSHClient()
-#     hostname='98.210.69.250'
-#     port=22
-#     username='pi'
-#     password='Felicia2020#'
-#     
-#     
-#     client.set_missing_host_key_policy(paramiko.AutoAddPolicy()) 
-#     client.load_system_host_keys()
-#     print('loaded client')
-#     client.connect(hostname, port, username, password)
-#     
-#     sftp_client = client.open_sftp()
-#     
-#     localFilePath='./detector_output.json'
-#     sftp_client.get('/home/pi/water_logger/detector_output.json', localFilePath)
-#         
-#     sftp_client.close() 
-#     
-#     
-#     with open(localFilePath) as detector_output:
-#       flow_data = json.load(detector_output)
-#   
-#     flow_eval=str(flow_data['flow'])
-# =============================================================================
+    
+    client =  paramiko.client.SSHClient()
+    hostname='98.210.69.250'
+    port=22
+    username='pi'
+    password='Felicia2020#'
+    
+    
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy()) 
+    client.load_system_host_keys()
+    print('loaded client')
+    client.connect(hostname, port, username, password)
+    
+    sftp_client = client.open_sftp()
+    
+    localFilePath='./detector_output.json'
+    sftp_client.get('/home/pi/water_logger/detector_output.json', localFilePath)
+        
+    sftp_client.close() 
+    
+    
+    with open(localFilePath) as detector_output:
+      flow_data = json.load(detector_output)
+  
+    flow_eval=str(flow_data['flow'])
     
     connection = psycopg2.connect(user = "beef",
                                   password = "Felicia2020#",
@@ -115,12 +113,10 @@ def serve_layout():
             'textAlign': 'left',
             'color': colors['text']}),
         
-# =============================================================================
-#         html.Div(children=f'Flow: {flow_eval}',            
-#             style={
-#             'textAlign': 'left',
-#             'color': colors['text']}),        
-# =============================================================================
+        html.Div(children=f'Flow: {flow_eval}',            
+            style={
+            'textAlign': 'left',
+            'color': colors['text']}),        
         
         dcc.Graph(id='example-graph',
               figure={'data': [{'x': init_df.record_date, 'y': init_df.gallons, 'type': 'line', 'name': 'Gallons'}],
