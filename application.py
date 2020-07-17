e7097894e7be430b75b7f216ede4b29b2ef782c0
Vahan Mouradian <vahanmouradian@outlook.com>
2020-07-17 12:19:18 -0700

    test

--- application.py
+++ application.py
@@ -58,33 +58,31 @@ def serve_layout():
 
 
     
-# =============================================================================
-#     
-#     client =  paramiko.client.SSHClient()
-#     hostname='98.210.69.250'
-#     port=22
-#     username='pi'
-#     password='Felicia2020#'
-#     
-#     
-#     client.set_missing_host_key_policy(paramiko.AutoAddPolicy()) 
-#     #client.load_system_host_keys()
-#     print('loaded client')
-#     client.connect(hostname, port, username, password)
-#     
-#     sftp_client = client.open_sftp()
-#     
-#     localFilePath='./detector_output.json'
-#     sftp_client.get('/home/pi/water_logger/detector_output.json', localFilePath)
-#         
-#     sftp_client.close() 
-#     
-#     
-#     with open(localFilePath) as detector_output:
-#       flow_data = json.load(detector_output)
-#   
-#     flow_eval=str(flow_data['flow'])
-# =============================================================================
+    
+    client =  paramiko.client.SSHClient()
+    hostname='98.210.69.250'
+    port=22
+    username='pi'
+    password='Felicia2020#'
+    
+    
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy()) 
+    #client.load_system_host_keys()
+    print('loaded client')
+    client.connect(hostname, port, username, password)
+    
+    sftp_client = client.open_sftp()
+    
+    localFilePath='./detector_output.json'
+    sftp_client.get('/home/pi/water_logger/detector_output.json', localFilePath)
+        
+    sftp_client.close() 
+    
+    
+    with open(localFilePath) as detector_output:
+      flow_data = json.load(detector_output)
+  
+    flow_eval=str(flow_data['flow'])
     
     connection = psycopg2.connect(user = "beef",
                                   password = "Felicia2020#",
@@ -102,7 +100,6 @@ def serve_layout():
     total_gallons=round(sum(init_df.gallons))
   
     
-    flow_eval='YIP'
     
     
     timeline_graph = html.Div(style={'backgroundColor': colors['background']}, children=[
