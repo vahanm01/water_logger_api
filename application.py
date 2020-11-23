307a06f33e04c96002194d395eabe878859d8c2a
Vahan Mouradian <vahanmouradian@outlook.com>
2020-11-23 00:24:20 -0800

    new changes

--- application.py
+++ application.py
@@ -7,7 +7,7 @@ import numpy as np
 import dash
 import dash_html_components as html
 import dash_core_components as dcc
-import dash_table
+#import dash_table
 import paramiko
 import json
 import os
