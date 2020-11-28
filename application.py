7f53187d432134f732e5c77df3297790f93f10ed
Vahan Mouradian <vahanmouradian@outlook.com>
2020-11-27 21:08:49 -0800

    major changes

--- application.py
+++ application.py
@@ -3,37 +3,25 @@ import dash
 import dash_html_components as html
 import dash_core_components as dcc
 import os
+import datetime
+import dash_table
 from helpers import* 
 
 pi_pass = os.environ.get('pi_pass')
 pgres_pass = os.environ.get('pgres_pass')
 pi_hostname = os.environ.get('pi_hostname')
 
-
 application = Flask(__name__)
-
-# =============================================================================
-# @application.route('/waterlogger/')
-# def index():
-#     return 'Hello Flask app'
-# =============================================================================
-
-
 
 colors = {
     'background': '#111111',
     'text': '#7FDBFF'
 }
 
-
 app = dash.Dash(
     __name__,
     server=application
-   #routes_pathname_prefix='/waterlogger/'
 )
-
-#image_filename = 'hydrecon_stack.png'
-#encoded_image = base64.b64encode(open(image_filename, 'rb').read())
 
 def serve_layout():
     
@@ -43,33 +31,30 @@ def serve_layout():
     connection=pgres_com('beef', pgres_pass)
     init_df=pgres_query(f"select* from pulse_detection order by record_date asc", connection).data_frame
     
-    
     init_df["gallons"]=float(0)
     m_recent_df=init_df[init_df.record_date==max(init_df.record_date)]
     
-    
-    last_log=max(m_recent_df.record_date)
-    last_log=last_log.strftime("%Y %b %d %H:%M")
-    
+    active_log = datetime.datetime.strptime(flow_data['timestamp'], '%Y-%m-%d %H:%M:%S.%f')
+    active_log = active_log.strftime("%Y %b %d %H:%M")
     
     last_gallon=str(round(max(m_recent_df.total_pulses/26.7)))
     
-    #1800 based on average tested values in 2018, hall effect. 
     #New pulse detection is 13.3 pulses per gallon. We double because there are to reed switches at halfway.
-    init_df.gallons=init_df.total_pulses/26.7
+    init_df.gallons=round(init_df.total_pulses/26.7)
     total_gallons=round(sum(init_df.gallons))
     
     sums=init_df.resample('D', on='record_date', kind='period').sum()
     sums=sums[sums.total_pulses!=0]
     agd=round(sums.total_pulses.mean()/26.7)
     
-    
-    
-    graph_df=init_df.resample('D', on='record_date', kind='period').sum()
+    graph_df=init_df.resample('M', on='record_date', kind='period').sum()
     graph_df=graph_df[graph_df.gallons!=0]
+    graph_df['record_date']=graph_df.index.strftime("%Y/%m")
+    del graph_df['total_pulses']
+    graph_df=graph_df[['record_date', 'gallons']]
     
-    graph_df['record_date']=graph_df.index.to_series().astype(str)
-    
+    info_df={'LTD Gallons':str(total_gallons), 'Average Gallons /Day': str(agd), 'Active':flow_eval, 'Active log':active_log}
+    info_df=pd.DataFrame(info_df.items(), columns=['info_cell', 'value'])
 
     timeline_graph = html.Div( children=[
         html.H1(children='Hydrecon', 
@@ -78,54 +63,47 @@ def serve_layout():
             'font-family': 'Tahoma',
             'color': 'rgb(0, 128, 255)'}),
         
-       # html.Button('Stack', id='submit-val', n_clicks=0),
-
         dcc.Graph(id='example-graph',
               figure={'data': [{'x': graph_df.record_date, 'y': graph_df.gallons, 'type': 'bar', 'name': 'Gallons'}],
-                      'layout': {'title': 'Gallons'}}),
+                      'layout': {'title': 'Gallons', 'height': 400, 'width': 1000}}),
         
-
+        dash_table.DataTable(
+            id='info_table',
+            columns=[{"name": i, "id": i} for i in info_df.columns],
+            data=info_df.to_dict('records'),
+            style_table={
+                'whiteSpace': 'normal',
+                'width': 600,},
+            style_cell={'textAlign': 'left'},
+            style_header={
+                'backgroundColor': 'rgb(31,119,180)',
+                'fontWeight': 'bold',
+                'color': 'white'}
+            ),   
         
-        html.H3(children=f'Total gallons to date: {total_gallons}',            
-            style={
-            'textAlign': 'right',
-            'color': 'rgb(0, 0, 0)'}),
+        html.H4(children=''),
         
-        html.H4(children=f'Active flow: {flow_eval}',            
-            style={
-            'textAlign': 'right',
-            'color': 'rgb(0, 0, 0)'}),
-        
-        html.H4(children=f'Last log: {last_log} | {last_gallon} gallon(s)',            
-            style={
-            'textAlign': 'right',
-            'color': 'rgb(0, 0, 0)'}),
-        
-        html.H4(children=f'AGD: {agd}',            
-            style={
-            'textAlign': 'right',
-            'color': 'rgb(0, 0, 0)'})
-        
-# =============================================================================
-#         html.Img(src='data:image/png;base64,{}'.format(encoded_image.decode()),
-#                  style={
-#                         'style':"vertical-align:middle"}),
-# =============================================================================
+        dash_table.DataTable(
+            id='table',
+            columns=[{"name": i, "id": i} for i in graph_df.columns],
+            data=graph_df.to_dict('records'),
+            style_table={
+                'whiteSpace': 'normal',
+                'width': 600,},
+            style_cell={'textAlign': 'left'},
+            style_header={
+                'backgroundColor': 'gray',
+                'fontWeight': 'bold'}    
                 
+            )     
         ])
-    
-        
+
     return timeline_graph
     
 
-
-
 app.layout = serve_layout
 
-# run the app.
 if __name__ == "__main__":
 
-    
-    
-    #application.debug = True
+    application.debug = True
     application.run()
